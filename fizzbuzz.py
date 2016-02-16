f88703481fdb8291d96ffeacd388163e9905a72d
NihilNomen <michael.laifreedom@gmail.com>
2016-02-15 21:19:28 -0500

    should redo this with octocat printing instead filler

---
+++ fizzbuzz.py
@@ -0,0 +1,23 @@
+#FizzBuzz challenge
+
+name = raw_input("What is your name?")
+print "Hi, %s." % name
+
+print("Hello World! Welcome to the FizzBuzz Challenge!")
+
+varCount = 1
+while(varCount < 100):
+	print varCount
+	varCount = varCount + 1
+
+	if (varCount % 3 == 0) and (varCount % 5 == 0):
+		print("FizzBuzz")
+		varCount = varCount + 1
+
+	elif (varCount % 3) == 0:
+		print ("Fizz")
+		varCount = varCount + 1
+
+	elif (varCount % 5) == 0:
+		print ("Buzz")
+		varCount = varCount + 1
