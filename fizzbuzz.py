989a16b3b7cf6edb69150cfe9c8fbde1b137a4bf
Michael Lai <17228703+NihilNomen@users.noreply.github.com>
2022-02-25 21:12:06 -0500

    Update fizzbuzz.py
    Add Time Delay and Fizzbuzz Challenge Description

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,23 +1,32 @@
 #FizzBuzz challenge
+import time
 
-name = raw_input("What is your name?")
-print "Hello, %s." % name
+name = raw_input("What is your name? ")
 
-print("Hello World! Welcome to the FizzBuzz Challenge!")
+print("Hello %s! ") %(name) + ("Welcome to the FizzBuzz Challenge!")
+print (" ")
+
+print ("Fizzbuzz is a program which iterates the integers from 1 to a chosen number (In our case 100). For multiples of three print \"Fizz\" instead of the number and for the multiples of five print \"Buzz\". For numbers which are multiples of both three and five print \"FizzBuzz\"")
 
 varCount = 1
-while(varCount < 100):
+while(varCount < 101):
 	print varCount
 	varCount = varCount + 1
 
 	if (varCount % 3 == 0) and (varCount % 5 == 0):
 		print("FizzBuzz")
+		print(" ")
 		varCount = varCount + 1
+		time.sleep(0.5)
 
 	elif (varCount % 3) == 0:
 		print ("Fizz")
+		print(" ")
 		varCount = varCount + 1
+		time.sleep(0.5)
 
 	elif (varCount % 5) == 0:
 		print ("Buzz")
+		print(" ")
 		varCount = varCount + 1
+		time.sleep(0.5)
